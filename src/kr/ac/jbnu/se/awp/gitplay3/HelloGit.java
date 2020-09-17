8c1968b6555bde220880c2c58a155ff78c7863ec
kimhyojin123 <40828622+kimhyojin123@users.noreply.github.com>
2020-09-17 18:07:15 +0900

    add comment

--- src/kr/ac/jbnu/se/awp/gitplay3/HelloGit.java
+++ src/kr/ac/jbnu/se/awp/gitplay3/HelloGit.java
@@ -4,13 +4,10 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
 		System.out.println("kim sang woo");
-<<<<<<< HEAD
 		System.out.println("kim Hyun su");
 		System.out.println("kim Hyo Jin");
-=======
 		System.out.println("kim jung hun");
-		System.out.println("kim hyun su");
 		System.out.println("kim Bo gyung");
->>>>>>> refs/remotes/origin/master
+
 	}
 }
