a9c970630344634e53fe097d327434b60814913a
kimhyojin123 <40828622+kimhyojin123@users.noreply.github.com>
2020-09-17 17:51:05 +0900

    add comment

--- src/kr/ac/jbnu/se/awp/gitplay3/HelloGit.java
+++ src/kr/ac/jbnu/se/awp/gitplay3/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
 		System.out.println("Hello Git!!");
 		System.out.println("kim sang woo");
 		System.out.println("kim Hyun su");
+		System.out.println("kim Hyo Jin");
 	}
 }
